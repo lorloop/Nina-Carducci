46658a987427f1bd03d13db5dd5b83a6a078abd4
lorloop <gallet.julien@orange.fr>
2024-05-26 14:57:18 +0200

    correction du code pour les filtres et les fonctions prev / next

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -114,86 +114,30 @@
       }
     },
     openLightBox(element, lightboxId) {
+      const currentIndex = $(".gallery-item:visible").index(element);
       $(`#${lightboxId}`)
+        .data("currentIndex", currentIndex)
         .find(".lightboxImage")
         .attr("src", element.attr("src"));
       $(`#${lightboxId}`).modal("toggle");
     },
-    prevImage() {
-      let activeImage = null;
-      $("img.gallery-item").each(function() {
-        if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
-          activeImage = $(this);
-        }
-      });
-      let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
-      let imagesCollection = [];
-      if (activeTag === "all") {
-        $(".item-column").each(function() {
-          if ($(this).children("img").length) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      } else {
-        $(".item-column").each(function() {
-          if (
-            $(this)
-              .children("img")
-              .data("gallery-tag") === activeTag
-          ) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      }
-      let index = 0,
-        next = null;
-
-      $(imagesCollection).each(function(i) {
-        if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i ;
-        }
-      });
-      next =
-        imagesCollection[index] ||
-        imagesCollection[imagesCollection.length - 1];
-      $(".lightboxImage").attr("src", $(next).attr("src"));
+    prevImage(lightboxId) {
+      const lightbox = $(`#${lightboxId}`);
+      let currentIndex = lightbox.data("currentIndex");
+      const items = $(".gallery-item:visible");
+      currentIndex = (currentIndex - 1 + items.length) % items.length;
+      lightbox.data("currentIndex", currentIndex);
+      const newSrc = items.eq(currentIndex).attr("src");
+      lightbox.find(".lightboxImage").attr("src", newSrc);
     },
-    nextImage() {
-      let activeImage = null;
-      $("img.gallery-item").each(function() {
-        if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
-          activeImage = $(this);
-        }
-      });
-      let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
-      let imagesCollection = [];
-      if (activeTag === "all") {
-        $(".item-column").each(function() {
-          if ($(this).children("img").length) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      } else {
-        $(".item-column").each(function() {
-          if (
-            $(this)
-              .children("img")
-              .data("gallery-tag") === activeTag
-          ) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      }
-      let index = 0,
-        next = null;
-
-      $(imagesCollection).each(function(i) {
-        if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i;
-        }
-      });
-      next = imagesCollection[index] || imagesCollection[0];
-      $(".lightboxImage").attr("src", $(next).attr("src"));
+    nextImage(lightboxId) {
+      const lightbox = $(`#${lightboxId}`);
+      let currentIndex = lightbox.data("currentIndex");
+      const items = $(".gallery-item:visible");
+      currentIndex = (currentIndex + 1) % items.length;
+      lightbox.data("currentIndex", currentIndex);
+      const newSrc = items.eq(currentIndex).attr("src");
+      lightbox.find(".lightboxImage").attr("src", newSrc);
     },
     createLightBox(gallery, lightboxId, navigation) {
       gallery.append(`<div class="modal fade" id="${
@@ -240,7 +184,7 @@
         return;
       }
       $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
+      $(this).addClass("active active-tag");
 
       var tag = $(this).data("images-toggle");
 
